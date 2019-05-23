d2464d410d189103b8c31268079442ce430461df
Leon Tappe <ltappe@mail.uni-paderborn.de>
2019-05-23 14:21:07 +0200

    add database calls to book credit for a user

--- aufwerter.py
+++ aufwerter.py
@@ -27,11 +27,6 @@ db_host = db_url_split[2].split("@")[1]
 db_port = db_url_split[3].split("/")[0]
 db_name = db_url_split[3].split("/")[1]
 
-# create connection for later
-cnx = mysql.connector.connect(
-    user=db_user, password=db_pw, host=db_host, port=db_port, database=db_name)
-cursor = cnx.cursor()
-
 # INITIALIZE PAYPAL
 
 # credentials are loaded from environment variables
@@ -77,7 +72,7 @@ def success():
         payment_id = request.args.get("paymentId")
         token = request.args.get("token")
         payer_id = request.args.get("PayerID")
-        user_id = 12345  # TODO
+        user_id = int("37474")
     except:
         print("not enough valid query arguments")
         return Response(status=400, response="not enough query parameters specified for this")
@@ -90,10 +85,38 @@ def success():
 
     if payment.execute({"payer_id": payer_id}):
         print("Payment executed successfully")
-        add_transaction = ("INSERT INTO journal "
-                           "(user_id, credit, value, description, created) "
-                           "VALUES ({}, {}, {}, {}, {})".format(user_id, "", "", "", ""))
-        return render_template("success.html", id=payment.id)
+        cnx = mysql.connector.connect(
+            user=db_user, password=db_pw, host=db_host, port=db_port, database=db_name)
+        cursor = cnx.cursor()
+        get_last_credit = "SELECT credit FROM journal WHERE user_id={} ORDER BY id DESC LIMIT 1".format(
+            user_id)
+        cursor.execute(get_last_credit)
+
+        # parse the numbers for some math
+        try:
+            last_credit = int(cursor.fetchone()[0])
+        except (ValueError, TypeError) as err:
+            print("could not get last credit from db, ", err)
+            return Response(status=500, response="could not get credit from db")
+        try:
+            value = int(float(payment.transactions[0].amount.total))*100
+        except (ValueError, TypeError) as err:
+            print("could not get payment value from api, ", err)
+            return Response(status=500, response="could not get payment value from api")
+
+        print(last_credit, " + ", value, " = ", (last_credit+value))
+
+        add_transaction = "INSERT INTO journal (user_id, credit, value, description) VALUES ({}, {}, {}, 'Aufladung mit PayPal - {}')".format(
+            user_id, last_credit+value, value, payment_id)
+        cursor.execute(add_transaction)
+
+        update_user_credit = "UPDATE user SET credit={} WHERE id={}".format(
+            last_credit+value, user_id)
+        cursor.execute(update_user_credit)
+        cursor.close()
+        cnx.commit()
+        cnx.close()
+        return render_template("success.html", payment=payment)
     else:
         print(payment.error)  # Error Hash
         return Response(status=500, response="error while executing payment {}".format(payment_id))
@@ -130,7 +153,7 @@ def create_payment(price, user_id):
             {
                 "item_list": {
                     "items": [{
-                        "name": "top-{}-up".format(price),
+                        "name": "top-{}-up".format(price.split(".")[0]),
                         "sku": "item",
                         "price": price,
                         "currency": "EUR",
@@ -141,7 +164,7 @@ def create_payment(price, user_id):
                     "total": price,
                     "currency": "EUR",
                 },
-                "description": "Eine Aufladung von {}€ für deinen AStA Copyservice Account",
+                "description": "Eine Aufladung von {}€ für deinen AStA Copyservice Account".format(price),
                 "custom": user_id,
                 "payment_options": {
                     "allowed_payment_method": "INSTANT_FUNDING_SOURCE"
